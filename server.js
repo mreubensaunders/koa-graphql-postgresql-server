6b1e842d84ee2dd484db09a37f3db06519f85546
Michael Office <arcaneflorist@gmail.com>
2019-07-10 16:28:06 +0100

    enabled cors

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const Koa = require('koa');
 const mount = require('koa-mount');
 const graphqlHTTP = require('koa-graphql');
 const schema = require('./schema');
+const cors = require('@koa/cors');
 
 const server = new Koa();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // server.use(async ctx => {
 //   ctx.body = 'Hello World';
 // });
-
+server.use(cors());
 server.use(mount('/', graphqlHTTP({
   schema: schema,
   graphiql: true
